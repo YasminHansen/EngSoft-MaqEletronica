da884ab9fbc86b3ff1c255454c200914c1b51b7e
Yasmin Hansen <yaa-h@hotmail.com>
2020-05-27 19:58:57 -0300

    Modal da área restrita, fluxo do abastecedor. Ainda sem as funções JS e estilo CSS

--- Src/restrict-area.js
+++ Src/restrict-area.js
@@ -3,12 +3,23 @@
 ///     LOGIN
 ///     OPTION
 ///     REMOVE
+///ABASTECEDOR
+///     LOGIN
+///     OPTIONS
+///     ADD
+///     EDIT
+///     REMOVE
 function closeModal(){
     document.getElementById("area-restrict-home").style.display = "flex";
     document.getElementById("vendor-login").style.display= "none";
     document.getElementById("vendor-options").style.display= "none";
     document.getElementById("vendor-remove").style.display= "none";
     document.getElementById("vendor-add").style.display= "none";
+    document.getElementById("supplier-login").style.display= "none";
+    document.getElementById("supplier-options").style.display = "none";
+    document.getElementById("supplier-insert").style.display = "none";
+    document.getElementById("supplier-edit").style.display = "none";
+    document.getElementById("supplier-remove").style.display = "none";
 }
 
 //Funções das modais relacionadas ao VENDEDOR
@@ -30,4 +41,30 @@ function goToVendorRemove(){
 function goToVendorAdd(){
     document.getElementById("vendor-options").style.display= "none";
     document.getElementById("vendor-add").style.display= "block";
+}
+
+//Funções das modais relacionadas ao ABASTECEDOR
+function goToSupplierLogin(){
+    document.getElementById("supplier-login").style.display= "block";
+    document.getElementById("area-restrict-home").style.display = "none";
+}
+
+function goToSupplierOptions(){
+    document.getElementById("supplier-login").style.display= "none";
+    document.getElementById("supplier-options").style.display = "block";
+}
+
+function goToSupplierInsert(){
+    document.getElementById("supplier-options").style.display = "none";
+    document.getElementById("supplier-insert").style.display = "block";
+}
+
+function goToSupplierEdit(){
+    document.getElementById("supplier-options").style.display = "none";
+    document.getElementById("supplier-edit").style.display = "block";
+}
+
+function goToSupplierRemove(){
+    document.getElementById("supplier-options").style.display = "none";
+    document.getElementById("supplier-remove").style.display = "block";
 }
